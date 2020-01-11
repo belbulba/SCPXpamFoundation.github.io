ef3c8e982a5709912bafe1ba367de6db7065defb
belbulba <krasnovartem2001@gmail.com>
2020-01-11 22:31:32 +0300

    Web page was created.
    ver 0.1 alpha

---
+++ js/script.js
@@ -0,0 +1,154 @@
+function ChangeOn000() {
+  var elem = document.getElementById("name");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'>ДЕМЕНТИЙ</div>";
+
+  var elem = document.getElementById("image");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'><img src='images/nophoto.jpg' alt='' class='SCPImage''></div>";
+
+  var elem = document.getElementById("number");
+  elem.outerHTML = "<div id='number' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.1s'>Номер: #000</div>";
+
+  var elem = document.getElementById("level");
+  elem.outerHTML = "<div id='level' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.2s'>Допуск: четвёрты уровень</div>";
+
+  var elem = document.getElementById("classOf");
+  elem.outerHTML = "<div id='classOf' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.3s'>Класс: кетер</div>";
+
+  var elem = document.getElementById("age");
+  elem.outerHTML = "<div id='age' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.4s'>Возраст: [ДАННЫЕ УДАЛЕНЫ]</div>";
+
+  var elem = document.getElementById("date");
+  elem.outerHTML = "  <div id='date' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.5s'>Занесено в реестр: 08.01.20</div>";
+
+  var elem = document.getElementById("conditions");
+  var elemCopyConditions = document.getElementById("conditionsOf000");
+  elem.innerHTML = elemCopyConditions.innerHTML;
+
+  var elem = document.getElementById("description");
+  var elemCopyDescription = document.getElementById("descriptionOf000");
+  elem.innerHTML = elemCopyDescription.innerHTML;
+}
+
+function ChangeOn002() {
+  var elem = document.getElementById("name");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'>ОРТЁМ</div>";
+
+  var elem = document.getElementById("image");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'><img src='images/001Main.jpg' alt='' class='SCPImage''></div>";
+
+  var elem = document.getElementById("number");
+  elem.outerHTML = "<div id='number' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.1s'>Номер: #002</div>";
+
+  var elem = document.getElementById("level");
+  elem.outerHTML = "<div id='level' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.2s'>Допуск: первый уровень</div>";
+
+  var elem = document.getElementById("classOf");
+  elem.outerHTML = "<div id='classOf' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.3s'>Класс: безопасный</div>";
+
+  var elem = document.getElementById("age");
+  elem.outerHTML = "<div id='age' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.4s'>Возраст: ~18 столетий</div>";
+
+  var elem = document.getElementById("date");
+  elem.outerHTML = "  <div id='date' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.5s'>Занесено в реестр: 08.01.20</div>";
+
+  var elem = document.getElementById("conditions");
+  var elemCopyConditions = document.getElementById("conditionsOf002");
+  elem.innerHTML = elemCopyConditions.innerHTML;
+
+  var elem = document.getElementById("description");
+  var elemCopyDescription = document.getElementById("descriptionOf002");
+  elem.innerHTML = elemCopyDescription.innerHTML;
+}
+
+function ChangeOn001() {
+  var elem = document.getElementById("name");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'>СКВЕРНАЯ АРТЁМИЯ</div>";
+
+  var elem = document.getElementById("image");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'><img src='images/nophoto.jpg' alt='' class='SCPImage''></div>";
+
+  var elem = document.getElementById("number");
+  elem.outerHTML = "<div id='number' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.1s'>Номер: #001</div>";
+
+  var elem = document.getElementById("level");
+  elem.outerHTML = "<div id='level' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.2s'>Допуск: четвёртый уровень</div>";
+
+  var elem = document.getElementById("classOf");
+  elem.outerHTML = "<div id='classOf' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.3s'>Класс: кетер</div>";
+
+  var elem = document.getElementById("age");
+  elem.outerHTML = "<div id='age' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.4s'>Возраст: [ДАННЫЕ УДАЛЕНЫ]</div>";
+
+  var elem = document.getElementById("date");
+  elem.outerHTML = "  <div id='date' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.5s'>Занесено в реестр: 08.01.20</div>";
+
+  var elem = document.getElementById("conditions");
+  var elemCopyConditions = document.getElementById("conditionsOf001");
+  elem.innerHTML = elemCopyConditions.innerHTML;
+
+  var elem = document.getElementById("description");
+  var elemCopyDescription = document.getElementById("descriptionOf001");
+  elem.innerHTML = elemCopyDescription.innerHTML;
+}
+
+function ChangeOn005() {
+  var elem = document.getElementById("name");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'>РАЗДОР</div>";
+
+  var elem = document.getElementById("image");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'><img src='images/5main.jpg' alt='' class='SCPImage''></div>";
+
+  var elem = document.getElementById("number");
+  elem.outerHTML = "<div id='number' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.1s'>Номер: #005</div>";
+
+  var elem = document.getElementById("level");
+  elem.outerHTML = "<div id='level' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.2s'>Допуск: первый уровень</div>";
+
+  var elem = document.getElementById("classOf");
+  elem.outerHTML = "<div id='classOf' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.3s'>Класс: безопасный</div>";
+
+  var elem = document.getElementById("age");
+  elem.outerHTML = "<div id='age' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.4s'>Возраст: 4 года</div>";
+
+  var elem = document.getElementById("date");
+  elem.outerHTML = "  <div id='date' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.5s'>Занесено в реестр: 11.01.20</div>";
+
+  var elem = document.getElementById("conditions");
+  var elemCopyConditions = document.getElementById("conditionsOf005");
+  elem.innerHTML = elemCopyConditions.innerHTML;
+
+  var elem = document.getElementById("description");
+  var elemCopyDescription = document.getElementById("descriptionOf005");
+  elem.innerHTML = elemCopyDescription.innerHTML;
+}
+
+function ChangeOn012() {
+  var elem = document.getElementById("name");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'>ЛИНДЕМОМИЯ</div>";
+
+  var elem = document.getElementById("image");
+  elem.innerHTML = "<div class='wow bounceInDown' data-wow-duration='1s'><img src='images/5main.jpg' alt='' class='SCPImage''></div>";
+
+  var elem = document.getElementById("number");
+  elem.outerHTML = "<div id='number' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.1s'>Номер: #012</div>";
+
+  var elem = document.getElementById("level");
+  elem.outerHTML = "<div id='level' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.2s'>Допуск: <span class='fourthLevel'>четвёртый уровень</span> </div>";
+
+  var elem = document.getElementById("classOf");
+  elem.outerHTML = "<div id='classOf' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.3s'>Класс: <span class='keter overCrossed'>кетер</span><span class='thaumiel'> таумиель</span></div>";
+
+  var elem = document.getElementById("age");
+  elem.outerHTML = "<div id='age' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.4s'>Возраст: ~12 тыс. лет</div>";
+
+  var elem = document.getElementById("date");
+  elem.outerHTML = "  <div id='date' class='col-md-12  wow bounceInRight CatalogueFastInfo' data-wow-duration='1s' data-wow-delay='0.5s'>Занесено в реестр: 11.01.20</div>";
+
+  var elem = document.getElementById("conditions");
+  var elemCopyConditions = document.getElementById("conditionsOf012");
+  elem.innerHTML = elemCopyConditions.innerHTML;
+
+  var elem = document.getElementById("description");
+  var elemCopyDescription = document.getElementById("descriptionOf012");
+  elem.innerHTML = elemCopyDescription.innerHTML;
+}
